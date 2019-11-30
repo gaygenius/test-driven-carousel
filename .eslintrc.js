0f0eab58239a24e00e88a3c03d05f8c72b888418
Jose Capo <jose@josecapo.com>
2019-11-29 21:05:04 -0500

    :wrench: Eslint React config

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,4 +6,11 @@ module.exports = {
       jsx: true,
     },
   },
+  plugins: ['react'],
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  settings: {
+    react: {
+      version: '16.12.0',
+    },
+  },
 };
