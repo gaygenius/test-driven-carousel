a89d6908963091b126ef31c062f1a89b0d2a5423
Jose Capo <jose@josecapo.com>
2019-12-10 13:41:32 -0500

    :wrench: Initial webpack setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,9 @@ module.exports = {
   parser: 'babel-eslint',
   plugins: ['react'],
   extends: ['eslint:recommended', 'plugin:react/recommended'],
+  env: {
+    node: true,
+  },
   settings: {
     react: {
       version: '16.12.0',
